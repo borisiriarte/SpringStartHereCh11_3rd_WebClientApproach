3d133c8fd9303c0598d714d14ae98eb67ecd1ded
Boris2514 <borismiashiro@gmail.com>
2023-09-18 22:04:05 -0400

    Updating Doc :V

---
+++ src/test/java/com/example/main/SpringStartHereCh113rdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStartHereCh113rdApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
